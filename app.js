599d709ba742996e6701cc1f8ef15f52f199e37f
Eric Tenenbaum <erictenenbaum10@gmail.com>
2018-05-11 18:14:18 -0700

    added some more game functionality, need to smooth out some edge cases like clicking on the same pair again after they have been revealed

--- app.js
+++ app.js
@@ -2,7 +2,10 @@
 
     const game = {
         numberArray: [1, 1, 2, 2, 3, 3, 4, 4, 5],
-        revealedSquares: [],
+        revealedSquares: [], 
+        foundMatches: 0,
+        
+               
         shuffle: function (a) {
             var j, x, i;
             for (i = a.length - 1; i > 0; i--) {
@@ -44,10 +47,8 @@
                     console.log(this.revealedSquares);
 
                     if(this.revealedSquares.length === 2){
-                        console.log("Run checkForMatch()");
-                        setTimeout(function(){
-                            game.checkForMatch()
-                        }, 500);                        
+                        console.log("Run checkForMatch()");                      
+                        return this.checkForMatch();
                     }                   
                 }
               
@@ -55,55 +56,50 @@
 
         },
         checkForMatch: function(){
-            if(this.revealedSquares[0].dataShown === this.revealedSquares[1].dataShown){
-                console.log("same");
-               return this.revealedSquares = [];
+            if(this.revealedSquares[0].dataShown === this.revealedSquares[1].dataShown && this.revealedSquares[0].dataValue !== this.revealedSquares[1].dataValue){
+                console.log("same");                 
+                return this.checkForWin();
+           
             }
             else{
                 console.log("different");
                 console.log($("." + this.revealedSquares[0].dataValue)[0].children);
 
-                for(let i = 0; i < this.revealedSquares.length; i++) {
-                    $("." + this.revealedSquares[i].dataValue)[0].children[0].hidden = true;
-                    $("." + this.revealedSquares[i].dataValue).css("background-color", "white");
-                }
-
-               return this.revealedSquares = [];
-                
+                setTimeout(()=>{
+                    for(let i = 0; i < this.revealedSquares.length; i++) {
+                        $("." + this.revealedSquares[i].dataValue)[0].children[0].hidden = true;
+                        $("." + this.revealedSquares[i].dataValue).css("background-color", "white");
+                    }    
+                   return this.revealedSquares = [];
+                }, 500);                
             }
 
+            
+        },
+
+        checkForWin: function(){
+            this.foundMatches++
+            let totalMatches = ((this.numberArray.length - 1) / 2);
+            console.log("************************")
+            console.log(totalMatches);
+            console.log(totalMatches - this.foundMatches);
+
+            if(totalMatches - this.foundMatches > 0){
+                return this.revealedSquares = [];
+            }
+            else{
+                alert("You won the game!");
+            }
             
         }
     }
     $(".square").on("click", function () {
-        //    alert("hello");
-
         console.log($(this)[0].children[0].dataset.value);
         console.log($(this)[0].children[0].dataset.shown);
-        // console.log($(this)[0].children)
-        // console.log($(this)[0].children[0].hidden);
-
-
-        // $(this)[0].children[0].attr("hidden", "false");
-
-
-
-        // if($(this)[0].children[0].hidden){
-        //     $(this)[0].children[0].hidden = false;
-        // }
-        // else{
-        //     $(this)[0].children[0].hidden = true;
-        // }
-
-
-
         game.revealHidden($(this));
+    });
 
-
-    })
-
+    // Starts the game by generating the hidden numbers behind the squares
+    // generateHiddenNumbers takes in an array as a parameter. I pass in a function "shuffle" which returns a randomly shuffled array
     game.generateHiddenNumbers(game.shuffle(game.numberArray));
-
-   
-
 })();
